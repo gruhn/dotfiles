3e92587318a595b6d837e02aef9e53db8011ffe2
Niklas Gruhn <niklas.gruhn@gmail.com>
2018-01-08 15:25:21 +0100

    Remove Navim directory
    containing no custom config

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 ln -s `pwd`/.profile $HOME/.profile
-ln -s `pwd`/nvim $HOME/.config/
 ln -s `pwd`/.gitconfig $HOME/.gitconfig
 ln -s `pwd`/.navimrc $HOME/.navimrc
 ln -s `pwd`/redshift.conf $HOME/.config/redshift.conf
