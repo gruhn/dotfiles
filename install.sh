eb7d51f58ffc9212ad5259bac2056824758ff0d1
Niklas Gruhn <niklas.gruhn@gmail.com>
2018-01-08 14:30:30 +0100

    Add redshift config

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 ln -s `pwd`/.profile $HOME/.profile
-ln -s `pwd`/nvim $HOME/.config/nvim
+ln -s `pwd`/nvim $HOME/.config/
 ln -s `pwd`/.gitconfig $HOME/.gitconfig
 ln -s `pwd`/.navimrc $HOME/.navimrc
+ln -s `pwd`/redshift.conf $HOME/.config/redshift.conf
