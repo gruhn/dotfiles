fadac557a00086e17c10b387c564814abb5b9da4
Niklas Gruhn <niklas.gruhn@gmail.com>
2018-01-12 12:32:17 +0100

    Add zsh config file

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ ln -s `pwd`/.profile $HOME/.profile
 ln -s `pwd`/.gitconfig $HOME/.gitconfig
 ln -s `pwd`/.navimrc $HOME/.navimrc
 ln -s `pwd`/redshift.conf $HOME/.config/redshift.conf
+ln -s `pwd`/.zshrc $HOME/.zshrc
